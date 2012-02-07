d4abbf35ea50bae408c72a8cd58aa410cf9de9c2
Timo Heino <eraggo@gmail.com>
2012-02-07 12:57:46 +0200

    First commit. Here we go!

---
+++ main.cpp
@@ -0,0 +1,79 @@
+// Very ugly Brute-Force apt->RAPID converter
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+void comment(string);
+void linear(string);
+void arc(string);
+void replace(string&, string, string);
+
+ifstream in;
+
+int main(int argc, char ** argv)
+{
+	string rivi;
+
+	// First argument (file name) missing
+	if (argc < 2) {
+		cout << "Usage: " << argv[0] << " <filename>\n";
+		return -1;
+	}
+	
+	// Try to open file
+	in.open(argv[1]);
+	if (in.is_open()) {
+		cout << "File " << argv[1] << " opened succesfully\n";
+	} else {
+		cout << "Error opening file.\n";
+		return -1;
+	}
+
+	//TODO Make while loop
+	for (int i=0; i<50; i++) {
+		getline(in,rivi);
+		if(rivi[0]=='$')
+			comment(rivi);
+		else if(rivi.find("CIRCLE")!=string::npos)
+			arc(rivi);
+		else if(rivi.find("GOTO")!=string::npos)
+			linear(rivi);
+	}
+
+	in.close();
+	
+	return 0;
+}
+
+void comment(string rivi)
+{
+	cout << "Kommentti:" << rivi << endl;
+	return;
+}
+
+void linear(string rivi)
+{
+	replace(rivi, "GOTO / ", "");
+	rivi="MoveL " + rivi;
+	cout << rivi << endl;
+	
+}
+
+void arc(string rivi)
+{
+	cout << "****Found ARC\n";
+}
+
+/* 
+Function to replace parts of source string
+var r=string to being replacing s
+*/
+void replace(string& source, string s, string r)
+{
+	size_t found;
+	while (source.find(s)!=string::npos) {
+		found=source.find(s);
+		source.replace(found, s.length(), r);
+	}
+}
